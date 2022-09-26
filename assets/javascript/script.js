84cc84b1c5c21a8d686c055f850125d9fef09866
Sophie Carr <sophiemc99@gmail.com>
2022-09-26 14:12:35 -0400

    added 5day forecast function and localstorage function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,29 +1,25 @@
 var apiKey = '&APPID=9f142abae2a7a139bd1b6a03555f1348';
-//http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
 //Search bar variables
 var searchForm = document.querySelector('#searchForm')
 var cityInput = document.querySelector('#cityInput');
 var searchBtn = document.querySelector('#searchBtn');
 var searchHistory = document.querySelector('#searchHistoryBtnList');
-//Today's weather variables
-var currentCityName = document.querySelector('#currentCityName')
-var currentDate = document.querySelector('#currentDate')
-var currentIcon = document.querySelector('#currentIcon')
-var currentTemp = document.querySelector('#currentTemp')
-var currentWind = document.querySelector('#currentWind')
-var currentHumid = document.querySelector('#currentHumid')
 
-var cityName
 var lat
 var lon
 
+getSaved();
+
+ //Function to handle the submission
 var formSubmitHandler = function (event) {
     event.preventDefault();
   
-    cityName = cityInput.value.trim();
+     var cityName = cityInput.value.trim();
   
     if (cityName) {
       getWeather(cityName);
+      saveLocal(cityName);
   
     } else {
       alert('Please enter a location.');
@@ -44,39 +40,85 @@ function getWeather(cityName) {
         lat = (data[0].lat)
         lon = (data[0].lon)
     })
-
+  //Fetch to retrieve today's weather in the searched location
     .then(function() {
       fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + apiKey + '&units=imperial')
       .then(function(response) {
         return response.json();
       })
       .then(function(data) {
-        console.log(data)
+        console.log(data);
 
-        currentCityName.textContent = data.name
-        currentDate.textContent = (moment().format("dddd, MMMM Do, YYYY"))
-        currentTemp.textContent = 'Temperature: ' + data.main.temp + 'F'
-        currentWind.textContent = 'Wind: ' + data.wind.speed + 'MPH'
-        currentHumid.textContent = 'Humidity: ' + data.main.humidity + '%'
+        document.querySelector('#currentCityName').textContent = data.name;
+        document.querySelector('#currentDate').textContent = (moment().format("dddd, MMMM Do, YYYY"));
+        document.querySelector('#currentIcon').src =  'http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
+        document.querySelector('#currentTemp').textContent = 'Temperature: ' + data.main.temp + 'F';
+        document.querySelector('#currentWind').textContent = 'Wind: ' + data.wind.speed + 'MPH';
+        document.querySelector('#currentHumid').textContent = 'Humidity: ' + data.main.humidity + '%';
 
       })
 
     })
-
+  //Fetch to retrieve the 5-day weather information
     .then(function() {
       fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + apiKey + '&units=imperial')
       .then(function(response) {
         return response.json();
       })
       .then(function(data) {
-        console.log(data)
+        console.log(data);
 
-          
+        document.querySelector('#date1').textContent = (moment().add(1, 'days').calendar("dddd, MMMM Do, YYYY"));
+        document.querySelector('#icon1').src = 'http://openweathermap.org/img/wn/' + data.list[0].weather[0].icon + '@2x.png';
+        document.querySelector('#temp1').textContent = 'Temperature: ' + data.list[0].main.temp + 'F';
+        document.querySelector('#wind1').textContent = 'Wind: ' + data.list[0].wind.speed + 'MPH';
+        document.querySelector('#humid1').textContent = 'Humidity: ' + data.list[0].main.humidity + '%';
+
+        document.querySelector('#date2').textContent = (moment().add(2, 'days').calendar("dddd, MMMM Do, YYYY"));
+        document.querySelector('#icon2').src = 'http://openweathermap.org/img/wn/' + data.list[1].weather[0].icon + '@2x.png';
+        document.querySelector('#temp2').textContent = 'Temperature: ' + data.list[1].main.temp + 'F';
+        document.querySelector('#wind2').textContent = 'Wind: ' + data.list[1].wind.speed + 'MPH';
+        document.querySelector('#humid2').textContent = 'Humidity: ' + data.list[1].main.humidity + '%';
+
+        document.querySelector('#date3').textContent = (moment().add(3, 'days').calendar("dddd, MMMM Do, YYYY"));
+        document.querySelector('#icon3').src = 'http://openweathermap.org/img/wn/' + data.list[2].weather[0].icon + '@2x.png';
+        document.querySelector('#temp3').textContent = 'Temperature: ' + data.list[2].main.temp + 'F';
+        document.querySelector('#wind3').textContent = 'Wind: ' + data.list[2].wind.speed + 'MPH';
+        document.querySelector('#humid3').textContent = 'Humidity: ' + data.list[2].main.humidity + '%';
+
+        document.querySelector('#date4').textContent = (moment().add(4, 'days').calendar("dddd, MMMM Do, YYYY"));
+        document.querySelector('#icon4').src = 'http://openweathermap.org/img/wn/' + data.list[3].weather[0].icon + '@2x.png';
+        document.querySelector('#temp4').textContent = 'Temperature: ' + data.list[3].main.temp + 'F';
+        document.querySelector('#wind4').textContent = 'Wind: ' + data.list[3].wind.speed + 'MPH';
+        document.querySelector('#humid4').textContent = 'Humidity: ' + data.list[3].main.humidity + '%';
+
+        document.querySelector('#date5').textContent = (moment().add(5, 'days').calendar("dddd, MMMM Do, YYYY"));
+        document.querySelector('#icon5').src = 'http://openweathermap.org/img/wn/' + data.list[4].weather[0].icon + '@2x.png';
+        document.querySelector('#temp5').textContent = 'Temperature: ' + data.list[4].main.temp + 'F';
+        document.querySelector('#wind5').textContent = 'Wind: ' + data.list[4].wind.speed + 'MPH';
+        document.querySelector('#humid5').textContent = 'Humidity: ' + data.list[4].main.humidity + '%';
       })
 
     })
 
 };
+
+//Function to save to local storage
+function saveLocal(cityName) {
+
+  localStorage.setItem("city", JSON.stringify(cityName))
+
+}
+
+//Function to get info from local storage
+function getSaved() {
+   var cities = localStorage.getItem("city") || []
+
+   for (i = 0; i < cities.length; i++) {
+    var city = document.createElement("button")
+    city.innerText = cities[i].cityName
+    searchHistory.appendChild(city)
+}}
 
 
  searchBtn.addEventListener("click", formSubmitHandler)
